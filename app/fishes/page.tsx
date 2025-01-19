b65eb5584b8ebf907a17349edb74c0018e2b4999
unknown <luongcaophong1312@gmail.com>
2025-01-19 13:15:21 +0700

    fixed more window issue

--- app/fishes/page.tsx
+++ app/fishes/page.tsx
@@ -2,9 +2,11 @@
 import Fish from "@/components/fishes/fish/fish"
 
 export default async function Page() {
-  return (
-    <div className="absolute top-0 left-0 -z-[1000]">
-      <Fish startPosition={{x: 0, y: (Math.random() * window.innerWidth)}}></Fish>
-    </div>
-  )
+  if (typeof window !== "undefined") {
+    return (
+      <div className="absolute top-0 left-0 -z-[1000]">
+        <Fish startPosition={{x: 0, y: (Math.random() * window.innerWidth)}}></Fish>
+      </div>
+    )
+  }
 }
