c20304f28a9d3f72394063e6b5593ad01b398bb2
unknown <luongcaophong1312@gmail.com>
2025-01-19 13:20:27 +0700

    fixed even more window issue

--- app/fishes/page.tsx
+++ app/fishes/page.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
+
 import Fish from "@/components/fishes/fish/fish"
 
-export default async function Page() {
-  if (typeof window !== "undefined") {
-    return (
-      <div className="absolute top-0 left-0 -z-[1000]">
-        <Fish startPosition={{x: 0, y: (Math.random() * window.innerWidth)}}></Fish>
-      </div>
-    )
-  }
+
+export default function Page() {
+  return (
+    <div className="absolute top-0 left-0 -z-[1000]">
+      <Fish startPosition={{x: 0, y: (Math.random() * window.innerWidth)}}></Fish>
+    </div>
+  )
 }
