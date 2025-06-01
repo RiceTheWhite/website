cc2a9802728298ba0c45faac2137e318c11b557d
RiceTheWhite <96251071+RiceTheWhite@users.noreply.github.com>
2025-06-01 10:28:04 +0700

    Update page.tsx

--- components/sections/about/page.tsx
+++ components/sections/about/page.tsx
@@ -41,6 +41,7 @@ export default function About() {
       <p className="m-auto">roblox (i dont play very often): https://www.roblox.com/users/2573059566/profile</p>
 
       <p>also there are cool things in /fishes and /softbody</p>
+      <p>my listening habits in https://superduperdopewebsite.vercel.app/spotify</p>
 
 
       <div className="m-auto">
